d46ae61d9c03e5a415a7840cc75228c29707c502
bwees <brandonwees@gmail.com>
2021-01-25 19:01:26 -0500

    finished calendar tab ui

--- src/views/CalendarView.jsx
+++ src/views/CalendarView.jsx
@@ -1,6 +1,6 @@
 import { useTheme } from "@react-navigation/native";
 import React from "react";
-import { View, Text, SectionList } from "react-native";
+import { View, Text, SectionList, Dimensions } from "react-native";
 import { TextInput, TouchableOpacity, ScrollView, FlatList } from "react-native-gesture-handler";
 import { FONTS, SHADOW } from "../Theme";
 import Ionicons from 'react-native-vector-icons/Ionicons';
@@ -11,16 +11,21 @@ import { Keyboard } from 'react-native'
 import ListSeperator from "../components/ListSeperator";
 import AssignmentCell from "../components/AssignmentCell";
 import { getAssignmentsByDate, groupAssignmentsBy } from "../storage/StorageAPI";
-import { sortByDate, stringToDateObject } from "../Helpers";
-import { Calendar, CalendarList } from "react-native-calendars";
+import { stringToDateObject } from "../Helpers";
+import { CalendarList } from "react-native-calendars";
+import BottomSheet from 'reanimated-bottom-sheet';
+import Animated from 'react-native-reanimated';
+import { useRef } from "react";
+import LinearGradient from "react-native-linear-gradient";
 
 const CalendarView = (props) => {
 
     const { colors } = useTheme();
     const insets = useSafeAreaInsets();
 
-    const [groupedAssignments, setGroupedAssignments] = useState();
+    const [groupedAssignments, setGroupedAssignments] = useState([]);
     const [selectedDate, setSelected] = useState(new Date().toISOString().slice(0, 10))
+    const [numAssignments, setNumAssignments] = useState(0);
 
     const [markedDates, setMarkedDates] = useState(
         {
@@ -35,10 +40,6 @@ const CalendarView = (props) => {
     React.useEffect(() => {
         const refreshList = props.navigation.addListener('focus', () => {
             updateAssignmentList(selectedDate);
-            setMarkedDates({
-                [new Date().toISOString().slice(0, 10)]: { selected: true },
-            })
-            setSelected(new Date().toISOString().slice(0, 10))
         });
     }, [props.navigation]);
 
@@ -46,6 +47,15 @@ const CalendarView = (props) => {
         updateAssignmentList(selectedDate)
     }, [selectedDate])
 
+    React.useEffect(() => {
+        if (groupedAssignments[0]) {
+            setNumAssignments(groupedAssignments[0].data.length)
+        } else {
+            setNumAssignments(0)
+        }
+    }, [groupedAssignments])
+
+    const sheetRef = useRef();
 
     return (
         <View style={{ flex: 1 }}>
@@ -70,7 +80,7 @@ const CalendarView = (props) => {
                 </View>
                 <View height={1} style={{ borderRadius: 4, backgroundColor: colors.headerBorder, }} />
             </View>
-            <View style={{ height: 275, marginHorizontal: 12 }}>
+            <View style={{ marginHorizontal: 12 }}>
                 <CalendarList
                     onDayPress={(day) => {
                         const dateString = day.dateString
@@ -88,19 +98,54 @@ const CalendarView = (props) => {
                     }}
                 />
             </View>
-            <View height={1} style={{ borderRadius: 4, backgroundColor: colors.headerBorder }} />
-            <View style={{ flex: 1 }}>
-                <SectionList
-                    style={{ paddingHorizontal: 20 }}
-                    sections={groupedAssignments}
-                    keyExtractor={(item, index) => item + index}
-                    stickySectionHeadersEnabled={false}
-                    renderItem={({ item }) => <AssignmentCell assignment={item} navigation={props.navigation} />}
-                    renderSectionHeader={({ section: { title } }) => (
-                        <ListSeperator icon={"calendar"} label={new Date(title).toLocaleString('default', { month: 'long', day: "numeric" })} color={colors.primary} bgColor={colors.listSeperator} />
-                    )}
-                />
-            </View>
+            <BottomSheet
+                ref={sheetRef}
+                snapPoints={[Dimensions.get("window").height - insets.top * 2 - 32, 85]}
+                initialSnap={1}
+                borderRadius={10}
+                scrollEnabled={false}
+                renderContent={() =>
+                    <View style={[SHADOW, { height: Dimensions.get("window").height - insets.top * 2 - 32, overflow: "visible" }]}>
+                        <LinearGradient
+                            start={{ x: 0, y: 0 }}
+                            end={{ x: 0, y: .1 }}
+                            colors={["#4287f5", "#40d6ff",]}
+                            useAngle={true}
+                            angle={120}
+                            style={{ flex: 1, paddingTop: 8 }}
+                        >
+                            <View style={{
+                                alignSelf: "center",
+                                width: 50,
+                                height: 6,
+                                borderRadius: 4,
+                                backgroundColor: "white",
+                            }} />
+
+                            <View style={{ paddingHorizontal: 20, paddingVertical: 8 }}>
+                                <Text style={[FONTS.h2, FONTS.bold, { color: "white" }]}>
+                                    {
+                                        stringToDateObject(selectedDate)
+                                            .toLocaleDateString(undefined, { day: "numeric", month: "long" })
+                                    }
+                                </Text>
+                                <Text style={[FONTS.h3, { color: "white" }]}>{numAssignments} Assignments</Text>
+                            </View>
+                            <View height={1} style={{ borderRadius: 4, marginTop: 4, backgroundColor: "white" }} />
+
+                            <SectionList
+                                style={{ paddingHorizontal: 20, marginTop: 16, flex: 1 }}
+                                sections={groupedAssignments}
+                                keyExtractor={(item, index) => item + index}
+                                stickySectionHeadersEnabled={false}
+                                renderItem={({ item }) => <AssignmentCell assignment={item} navigation={props.navigation} />}
+
+                            />
+                        </LinearGradient>
+                    </View>
+                }
+            />
+
         </View>
     );
 };
