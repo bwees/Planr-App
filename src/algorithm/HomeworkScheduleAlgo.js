f2e912c0866e5fcb9c3cf7892b92a567e9aa0611
bwees <brandonwees@gmail.com>
2021-03-06 18:24:31 -0500

    started work on algo

--- src/algorithm/HomeworkScheduleAlgo.js
+++ src/algorithm/HomeworkScheduleAlgo.js
@@ -1,3 +1,14 @@
+const BinPacking2D = require('binpackingjs').BP2D;
+const { Bin, Box, Packer } = BinPacking2D;
+
 export function generateHomeworkSchedule(assignments, workTimes) {
-    console.log(assignments, workTimes)
+
+    assignmentBoxes = []
+    workBoxes = []
+
+    assignments.forEach(function (item, index) {
+        assignmentBoxes.push(new Box(item.time, 1))
+    });
+
+    
 }
--- src/storage/StorageSchema.js
+++ src/storage/StorageSchema.js
@@ -1,30 +1,39 @@
-
-// Define your models and their properties
 const AssignmentSchema = {
-    name: 'Assignment',
-    primaryKey: 'id',
+    name: "Assignment",
+    primaryKey: "id",
     properties: {
-        name: 'string',
-        className: 'string',
-        type: 'string',
-        dueDate: 'date',
-        time: { type: 'int', default: 15 },
-        status: { type: 'int', default: 0 },
-        notes: 'string',
-        attachments: 'File[]',
+        name: "string",
+        className: "string",
+        type: "string",
+        dueDate: "date",
+        time: { type: "int", default: 15 },
+        status: { type: "int", default: 0 },
+        notes: "string",
+        attachments: "File[]",
         id: "string"
     }
 };
 
 const FileSchema = {
-    name: 'File',
-    primaryKey: 'id',
+    name: "File",
+    primaryKey: "id",
     properties: {
-        name: 'string',
+        name: "string",
         type: "string",
-        path: 'string',
-        id: 'string'
+        path: "string",
+        id: "string"
     }
 };
+
+const WorkTime = {
+    name: "WorkTime",
+    primaryKey: "id",
+    properties: {
+        name: "string",
+        start: "string",
+        end: "string",
+        minutes: "int"
+    }
+}
 
 export const schema = [AssignmentSchema, FileSchema]
