bdeeb994243b897c5b648849c79976959fe38a99
bwees <brandonwees@gmail.com>
2021-03-20 13:28:06 -0400

    added expanding work times options

--- src/algorithm/HomeworkScheduleAlgo.js
+++ src/algorithm/HomeworkScheduleAlgo.js
@@ -10,7 +10,9 @@ export function generateHomeworkSchedule(assignments, workTimes) {
 
     assignments.forEach(function (item) {
         assignmentBoxes.push(new Item(item.time, item))
-        usedTime += item.time
+        if (item.status != 2) {
+            usedTime += item.time
+        }
     });
     assignmentBoxes.sort((a, b) => (a.width < b.width) ? 1 : -1) // sort by largest
 
@@ -23,6 +25,26 @@ export function generateHomeworkSchedule(assignments, workTimes) {
     let packer = new Packer(workBins, assignmentBoxes);
     packer.pack();
 
+    // Pack unfit items into expandable work times
+    packer.unpacked.forEach(item => {
+        workBins.some((bin) => {
+            if (bin.data.canExpand) {
+                bin.add(item)
+                console.log("Packed oversized " + item.data.name + " into " + bin.data.name)
+                return true
+            }
+        })
+    })
+
+    // Pack unfit items into seperate section
+    if (packer.unpacked.length > 0) {
+        oversizeBin = new Bin(-1, {name: "Unfit Assignments"})
+        packer.unpacked.forEach(item => {
+            oversizeBin.add(item)
+        })
+        workBins.push(oversizeBin)
+    }
+    
     
     // create data structure for sectionList
     schedule = []
@@ -30,6 +52,11 @@ export function generateHomeworkSchedule(assignments, workTimes) {
     workBins.forEach(function(item) {
         var b = {}
         b.title = item.data.name
+        
+        if (item.remaining < 0) {
+            b.title += " (" + Math.abs(item.remaining) + " Minutes Over)"
+        } 
+
         b.data = []
         item.packed.forEach((item) => {
             b.data.push(item.data)
